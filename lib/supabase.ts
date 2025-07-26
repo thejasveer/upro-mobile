7df9149813987f5a53105528cdb590cae493f818
thejasveer13 <thejasveer13@gmail.com>
2025-07-26 04:48:08 -0500

    Updated styles in auth screens

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || "YOUR_SUPABASE_URL";
 const supabaseAnonKey =
   process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || "YOUR_SUPABASE_ANON_KEY";
-
+console.log(supabaseUrl);
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
